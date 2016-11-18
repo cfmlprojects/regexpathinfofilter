128edc2f4fba855826fb25258a216a170a281344
cfmlprojects <cfml@cfmlprojects.org>
2016-11-17 21:11:30 -0700

    initial go at it

---
+++ src/java/org/cfmlprojects/regexpathinfofilter/RegexPathInfoFilter.java
@@ -0,0 +1,56 @@
+package org.cfmlprojects.regexpathinfofilter;
+
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+
+public class RegexPathInfoFilter implements Filter {
+
+    private static Pattern regex;
+    private static Matcher matcher;
+    private static final String REGEX_DEFAULT = "^(/.+\\.cf[cm])(/.*)";
+    private static Logger logger = Logger.getLogger("org.cfmlprojects.regexpathinfofilter");
+
+    @Override
+    public void init(FilterConfig config) throws ServletException {
+        final String regexString = config.getInitParameter("regex") != null ? config.getInitParameter("regex") : REGEX_DEFAULT;
+        regex = Pattern.compile(regexString);
+        matcher = regex.matcher("");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws ServletException, IOException {
+        final String requestURI = ((HttpServletRequest)req).getRequestURI();
+        if (matcher.reset(requestURI).matches()) {
+            final String newURL = matcher.group(1);
+            final String path_info = matcher.group(2);
+            logger.log(Level.FINE,requestURI + " matches " + REGEX_DEFAULT + " regex, now: " + newURL + " path_info: " + path_info);
+            final HttpServletRequestWrapper wrapped = new HttpServletRequestWrapper( (HttpServletRequest)req) {
+                @Override
+                public String getPathInfo() {
+                    return path_info;
+                }
+            };
+            req.getRequestDispatcher(newURL).forward(wrapped, res);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        //
+    }
+}
